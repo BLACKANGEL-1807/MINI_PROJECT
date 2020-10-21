7163affd0c481ddf339da584d95a27cdd9437e51
VIKRAM <vickknguyen123@gmail.com>
2020-10-22 01:50:47 +0530

    Login Page Enhanced

--- LOGIN.py
+++ LOGIN.py
@@ -18,20 +18,20 @@ def Frame_Login():
     frame.grid(padx=100, pady=100, sticky=N)
 
     # User-Name Index 
-    UserName_label = Label(frame, text="UserName:*", padx=2, pady=5, justify=RIGHT).grid(row=0,column=0, padx=1, pady=5)
+    UserName_label = Label(frame, text="UserName:*", padx=2, pady=5, justify=RIGHT, font=("Times", 13, "bold")).grid(row=0,column=0, padx=1, pady=5)
     E = Entry(frame, width=50, borderwidth=3)
     E.grid(row=0, column=1, columnspan=3, padx=10, pady=10)
 
     # Password_Index
-    Password_label = Label(frame, text="Password:*", padx=2, pady=5, justify=RIGHT).grid(row=1,column=0, padx=1, pady=5)
+    Password_label = Label(frame, text="Password:*", padx=2, pady=5, justify=RIGHT, font=("Times", 13, "bold")).grid(row=1,column=0, padx=1, pady=5)
     E1 = Entry(frame, width=50, borderwidth=3, show="*")
     E1.grid(row=1, column=1, columnspan=3, padx=10, pady=10)
 
     # BUTTONS -Submit & -New_User
-    Submit_Button = Button(frame, text=" SUBMIT ", padx=10, pady=5, borderwidth=4, command=loginmessage)
+    Submit_Button = Button(frame, text=" SUBMIT ", padx=10, pady=5, borderwidth=4, command=loginmessage, cursor="hand2", font=("Verdana",12, "underline"), bd=0)
     Submit_Button.grid(row=2, column=2, padx=30, pady=30) 
 
-    New_User_Button = Button(frame, text=" New User ", padx=5, pady=5, borderwidth=4)
+    New_User_Button = Button(frame, text=" NewUser ", padx=5, pady=5, borderwidth=4, cursor="hand2", font=("Verdana",12, "underline"), bd=0)
     New_User_Button.grid(row=2, column=3, padx=30, pady=30) 
     
     root.mainloop()
